0b9d24b4633a1c7909cfe834a863a7b538665336
Aluno <you@example.com>
2019-07-18 20:32:10 -0300

    Criando e implementando o metodo detail.

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-detail',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
+  id;
+  avaliacao;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { 
+    this.id = this.route.snapshot.paramMap.get('id')
+    this.avaliacao = this.http.get("http://5d262d00eeb36400145c59b3.mockapi.io/avaliacao/" + this.id).subscribe(
+      (data) => {
+        this.avaliacao = data
+      }
+    )
+  }
 
   ngOnInit() {
   }
